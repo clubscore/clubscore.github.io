2ae4655cf769a35347186b8a1a5092127c39f7a5
sanja <sanjay.kumar.jha@live.com>
2024-11-24 21:16:15 +0530

    game updates

--- scoreboard.js
+++ scoreboard.js
@@ -17,25 +17,7 @@ const firebaseConfig = {
     measurementId: "G-BJTG3WZMSC"
 };
 
-// debugger;
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-console.log(app.name);
-const db = getFirestore(app);
-console.log(db);
-
-getDoc(doc(db, "scores", "GSh0R9fAcLdFKgPuYR0S")).then(testDoc=>{
-    if(testDoc.exists())
-    {
-        console.log("document found");
-        console.log(testDoc.data());
-    }
-    else
-    {
-        console.log("document missing");
-    }
-})
-
+var gameId;
 
 function getQueryStringParams() {
     const queryString = window.location.search;
@@ -45,30 +27,55 @@ function getQueryStringParams() {
     for (const [key, value] of urlParams.entries()) {
       params[key] = value;
     }
-  
+
     return params;
-  }
-  
-  const queryParams = getQueryStringParams();
-  console.log(queryParams.id);
+}
+
+function setGameId() {
+    const queryParams = getQueryStringParams();
+
+    if(queryParams?.id)
+    {
+        console.log("game id -"+queryParams.id);        
+        console.log("saved game id to localStorage");        
+        localStorage.setItem("gameId", queryParams.id);
+        gameId = queryParams.id;
+    }
+    else
+    {
+        gameId = localStorage.getItem("gameId");
+        console.log("game id not found in url. local-"+gameId);
+    }
+}
+
+setGameId();
+// debugger;
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+console.log(app.name);
+const db = getFirestore(app);
+console.log(db);
 
 function getDataFromDbs()
 {
     console.log("method was called");
 }
 
-onSnapshot(doc(db, "scores", "GSh0R9fAcLdFKgPuYR0S"), 
+onSnapshot(doc(db, "scores", gameId), 
     testDoc =>     {
-        console.log("document updated");
-        console.log(testDoc.data());
-        console.log(testDoc.data()["gameScore"]["pointSide1"]);
-        var pointCell = document.getElementById("pointSide1");
-        pointCell.textContent=testDoc.data()["gameScore"]["pointSide1"];
-        var pointCell = document.getElementById("pointSide2");
-        pointCell.textContent=testDoc.data()["gameScore"]["pointSide2"];
+        if(testDoc.exists())
+        {
+            console.log("document updated");
+            // console.log(testDoc.data());
+            // console.log(testDoc.data()["pointSide1"]);
+            updateScore(testDoc);
+        }
+        else
+        {
+            console.log("game not found - "+gameId);
+        }
     }
 );
-
 
 const message = () => {
     const name = "Jesse";
@@ -80,3 +87,48 @@ const message = () => {
   export {
     getDataFromDbs
   }
+
+function updateScore(testDoc) {
+    var pointCell = document.getElementById("pointSide1");
+    pointCell.textContent = testDoc.data()["pointSide1"];
+    var pointCell = document.getElementById("pointSide2");
+    pointCell.textContent = testDoc.data()["pointSide2"];
+    clearPrevGameScores();
+    var prevGameSide1 = testDoc.data()["prevGameSide1"];
+    var prevGameSide2 = testDoc.data()["prevGameSide2"];
+    
+    var gameSide1 = testDoc.data()["gameSide1"];
+    var gameSide2 = testDoc.data()["gameSide2"];
+
+    updateGames(prevGameSide1, "playerNameSide1", gameSide1);
+
+    updateGames(prevGameSide2, "playerNameSide2", gameSide2);
+}
+
+function updateGames(prevGames, identifier, gameCurr)
+{
+    insertCell(identifier, gameCurr);
+
+    for (let i = prevGames.length-1; i>=0; i--) {
+        insertCell(identifier, prevGames[i]);
+    }
+}
+
+function clearPrevGameScores(){
+    const elements = document.getElementsByName("prevGame");
+    var length = elements.length;
+    if(length>0)
+    {
+        for (let i = length-1; i >= 0; i--) {
+            elements[i].parentNode.removeChild(elements[i]);
+        }
+    }
+}
+
+function insertCell(identifier, score) {
+    var cell = document.createElement("td");
+    cell.textContent = score;
+    cell.setAttribute("name", "prevGame");
+    document.getElementById(identifier).insertAdjacentElement('afterend', cell);
+}
+
